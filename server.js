ad8af025fa65b1d3b0708279c44226af090726cc
dien <iamdiennn@gmail.com>
2019-06-20 00:33:31 -0700

    install packages and server.js

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const mongojs = require("mongojs");
+const axios = require("axios");
+const cheerio = require("cheerio");
+
+
+
+const app = express()
+
+const PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT)
+  })
+  
