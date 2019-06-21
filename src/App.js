f2ce49dbe4c10e554123346098e1629dc69c4ceb
dien <iamdiennn@gmail.com>
2019-06-20 23:43:11 -0700

    issues with axios in react

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import axios from 'axios'
+import cheerio from 'cheerio'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends Component {
+  state = {
+    news: ''
+  }
+
+  handleGetArticles = event =>{
+    console.log('ping')
+    axios.get('https://www.nytimes.com/section/world/asia')
+      .then(({data})=>{
+        const $ = cheerio.load(data)
+        console.log(data)
+      })
+  }
+  render(){
+    return(
+      <>
+      <h1>React Mongo Scrappppinggg</h1>
+      <button onClick={this.handleGetArticles}>Get Articles</button>
+      </>
+      )
+    }
+  }
+
 
 export default App;
